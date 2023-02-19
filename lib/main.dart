e447fb51cf8a86129ac31722a59b2516b6ee04ec
Matthew Segura <matthewsegura@Matthews-MBP-2.attlocal.net>
2023-02-18 19:13:27 -0600

    added search and bottom nav changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_launcher_icons/ios.dart';
 import 'second_page.dart';
 
 void main() {
@@ -33,22 +34,54 @@ class FirstPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("MealAI"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
+      body: Stack(
         children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                // adding new page
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const SecondPage();
-                    },
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+              decoration: const BoxDecoration(
+                color: Color(0xFF0066EE),
+                borderRadius: BorderRadius.only(
+                    // topLeft: Radius.circular(20.0),
+                    // topRight: Radius.circular(20.0),
+                    ),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.all(8.0),
+                      ),
+                    ),
                   ),
-                );
-              },
-              child: const Text('Scan Food Item'),
+                  const SizedBox(width: 8.0),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // adding new page
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return SecondPage();
+                          },
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.qr_code_scanner),
+                    label: Text('Scan'),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
@@ -69,3 +102,20 @@ class FirstPage extends StatelessWidget {
     );
   }
 }
+
+
+/*
+bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        backgroundColor: Color(0xFF0066EE),
+        selectedItemColor:
+            Colors.white, // set the color of the selected icon and label
+        unselectedItemColor:
+            Colors.white, // set the color of the unselected icon and label
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: 'Diary', icon: Icon(Icons.book)),
+          BottomNavigationBarItem(label: 'More', icon: Icon(Icons.more))
+        ],
+      ), 
+*/
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -6,9 +6,19 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
+  SecondPage({super.key});
 
-  Future<void> getProductInfo(String? barcode) async {
+  List<String> foodItems = [];
+
+  bool addFoodItem(String productName) {
+    if (foodItems.contains(productName)) {
+      return false; // Product is already in the list
+    }
+    foodItems.add(productName);
+    return true; // Product added to the list
+  }
+
+  Future<String?> getProductInfo(String? barcode) async {
     if (barcode != null) {
       final url =
           'https://world.openfoodfacts.org/api/v0/product/${barcode}.json';
@@ -16,14 +26,15 @@ class SecondPage extends StatelessWidget {
       if (response.statusCode == 200) {
         final product = json.decode(response.body)['product'];
         final productName = product['product_name'];
-        // final productImageUrl = product['image_front_url'];
-        debugPrint('Product Name: $productName');
-        // debugPrint('Product Image URL: $productImageUrl');
+        // debugPrint('Product Name: $productName');
+        return productName;
       } else {
         debugPrint('Failed to fetch product information');
+        return null;
       }
     } else {
       debugPrint('Barcode is null');
+      return null;
     }
   }
 
@@ -32,7 +43,7 @@ class SecondPage extends StatelessWidget {
     // This builds a new widget, which consists of a Scaffold.
     return Scaffold(
       // The Scaffold has an AppBar at the top.
-      appBar: AppBar(title: const Text('Food Scan')),
+      appBar: AppBar(title: const Text('Meal AI')),
       // The body of the Scaffold is a MobileScanner widget.
       body: MobileScanner(
         // When a barcode is detected, the onDetect callback is triggered.
@@ -43,7 +54,15 @@ class SecondPage extends StatelessWidget {
           for (final barcode in barcodes) {
             // get the barcode raw value
             // debugPrint('Barcode found! ${barcode.rawValue}');
-            await getProductInfo(barcode.rawValue);
+            final productName = await getProductInfo(barcode.rawValue);
+            if (productName != null) {
+              final added = addFoodItem(productName);
+              if (added) {
+                debugPrint('Product added to list: $productName');
+              } else {
+                debugPrint('Product already in list: $productName');
+              }
+            }
           }
         },
       ),
