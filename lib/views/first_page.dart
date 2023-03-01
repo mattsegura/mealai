48753efc3f82a6e936b456ca473f7fedbe13c080
Matthew Segura <matthewsegura@Matthews-MBP-2.attlocal.net>
2023-02-28 19:28:14 -0600

    finished macro status widget

--- lib/views/first_page.dart
+++ lib/views/first_page.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:newapp/models/food_items_model.dart';
 import 'second_page.dart';
 import 'meal_plan.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,6 +47,7 @@ class FirstPage extends StatelessWidget {
                 'Today',
                 style: TextStyle(
                   color: Colors.black, // set the color of the title text
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               IconButton(
@@ -85,6 +87,131 @@ class FirstPage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Row(
+                      children: const [
+                        Text(
+                          "Macros",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircularPercentIndicator(
+                          animation: true,
+                          animationDuration: 1000,
+                          radius: 50,
+                          lineWidth: 10,
+                          percent: 0.6,
+                          progressColor: Colors.blue,
+                          backgroundColor: Colors.blue.shade100,
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 30.0),
+                            child: Column(
+                              children: const [
+                                Text(
+                                  '120',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Carbs',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        CircularPercentIndicator(
+                          animation: true,
+                          animationDuration: 1000,
+                          radius: 50,
+                          lineWidth: 10,
+                          percent: 0.8,
+                          progressColor: Color.fromRGBO(253, 129, 205, 1),
+                          backgroundColor: Color.fromRGBO(253, 129, 205, 450),
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 30.0),
+                            child: Column(
+                              children: const [
+                                Text(
+                                  '150',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Protein',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        CircularPercentIndicator(
+                          animation: true,
+                          animationDuration: 1000,
+                          radius: 50,
+                          lineWidth: 10,
+                          percent: 0.4,
+                          progressColor: Color.fromRGBO(255, 193, 26, 1),
+                          backgroundColor: Color.fromRGBO(255, 193, 26, 450),
+                          circularStrokeCap: CircularStrokeCap.round,
+                          center: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 30.0),
+                            child: Column(
+                              children: const [
+                                Text(
+                                  '216',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'Fats',
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           Expanded(
             child: Visibility(
               visible: foodItemsModel.foodItems.isNotEmpty,
@@ -113,55 +240,52 @@ class FirstPage extends StatelessWidget {
               ),
             ),
           ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
-              decoration: const BoxDecoration(
-                color: Color(0xFF0066EE),
-                borderRadius: BorderRadius.only(),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onSubmitted: (value) {
-                        foodItemsModel.addFoodItem(value);
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Search for food",
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        filled: true,
-                        fillColor: Colors.white,
-                        contentPadding: const EdgeInsets.all(8.0),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            decoration: const BoxDecoration(
+              color: Color(0xFF0066EE),
+              borderRadius: BorderRadius.only(),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    onSubmitted: (value) {
+                      foodItemsModel.addFoodItem(value);
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Search for food",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: const EdgeInsets.all(8.0),
                     ),
                   ),
-                  const SizedBox(width: 8.0),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (BuildContext context) {
-                            return SecondPage(
-                              onFoodItemsUpdated: (foodItems) {
-                                // Add code here to handle the updated food items
-                              },
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    icon: Icon(Icons.qr_code_scanner),
-                    label: Text('Scan'),
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(width: 8.0),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return SecondPage(
+                            onFoodItemsUpdated: (foodItems) {
+                              // Add code here to handle the updated food items
+                            },
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.qr_code_scanner),
+                  label: Text('Scan'),
+                ),
+              ],
             ),
           ),
         ],
@@ -174,7 +298,7 @@ class FirstPage extends StatelessWidget {
         items: const [
           BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
           BottomNavigationBarItem(label: 'Meal Plan', icon: Icon(Icons.book)),
-          BottomNavigationBarItem(label: 'More', icon: Icon(Icons.more))
+          BottomNavigationBarItem(label: 'Settings', icon: Icon(Icons.more)),
         ],
         onTap: (index) {
           if (index == 1) {
